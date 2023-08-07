236ef6394ed350c5ec444c6ad2d873d9e36a779e
SushantaKumarSahoo <111504191+SushantaKumarSahoo@users.noreply.github.com>
2023-08-07 19:49:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import os
 
 class Config(object):
     # Mandotory
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    APP_ID = int(os.environ.get("23573562"))
+    API_HASH = os.environ.get("90c3fcac22ac68a5e78d4804942603f4")
+    BOT_TOKEN = os.environ.get("6024068299:AAEAAxAyrRaDmOLfqM23wmyliRq3fbDgX6o")
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
-    GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN")
+    BOT_OWNER = int(os.environ.get("5145748626"))
+    MONGODB_URL = os.environ.get("mongodb+srv://sksahoo153315:<Sa!Tg7vv1533>@cluster0.kqdabs0.mongodb.net/?retryWrites=true&w=majority")
+    GOFILE_TOKEN = os.environ.get("bEqPXWFPKDIxRFK2xs6kraxOYQuV8jjj")
     # Optional
     MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
     # Constents
